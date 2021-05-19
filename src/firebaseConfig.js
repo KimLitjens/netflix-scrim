ce89c41145c91a5041674cbe34bef3beeb04e31f
Kim Litjens <Kimlitjens4@gmail.com>
2021-05-19 20:02:29 +0200

    add firebaseConfig file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyAklqtZVjo0eyg7DUSNKgsUERFtAKqByGw",
+    authDomain: "netflix-kim.firebaseapp.com",
+    databaseURL: "https://netflix-kim-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "netflix-kim",
+    storageBucket: "netflix-kim.appspot.com",
+    messagingSenderId: "942139786727",
+    appId: "1:942139786727:web:4fcbdf7d1ea9936f406942",
+    measurementId: "G-M1WMDSNG8N"
+};
